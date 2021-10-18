8f371906df95f38689aad29261b0084f44791c1e
Eduardo S. Libardi <eslibardi@gmail.com>
2021-10-18 15:45:36 -0300

    project 11 - Auth Workflow - verify email, send email

--- 11-auth-workflow/starter/server/controllers/authController.js
+++ 11-auth-workflow/starter/server/controllers/authController.js
@@ -1,7 +1,12 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
-const { attachCookiesToResponse, createTokenUser } = require('../utils');
+const {
+  attachCookiesToResponse,
+  createTokenUser,
+  sendVerificationEmail,
+} = require('../utils');
+const crypto = require('crypto');
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -15,10 +20,28 @@ const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments({})) === 0;
   const role = isFirstAccount ? 'admin' : 'user';
 
-  const user = await User.create({ name, email, password, role });
-  const tokenUser = createTokenUser(user);
-  attachCookiesToResponse({ res, user: tokenUser });
-  res.status(StatusCodes.CREATED).json({ user: tokenUser });
+  const verificationToken = crypto.randomBytes(40).toString('hex');
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+    verificationToken,
+  });
+
+  const origin = 'http://localhost:3000';
+
+  await sendVerificationEmail({
+    name: user.name,
+    email: user.email,
+    verificationToken: user.verificationToken,
+    origin,
+  });
+
+  res.status(StatusCodes.CREATED).json({
+    msg: 'Success! Please check your email to verify account',
+  });
 };
 const login = async (req, res) => {
   const { email, password } = req.body;
@@ -35,6 +58,11 @@ const login = async (req, res) => {
   if (!isPasswordCorrect) {
     throw new CustomError.UnauthenticatedError('Invalid Credentials');
   }
+
+  if (!user.isVerified) {
+    throw new CustomError.UnauthenticatedError('Please verify your email');
+  }
+
   const tokenUser = createTokenUser(user);
   attachCookiesToResponse({ res, user: tokenUser });
 
@@ -48,8 +76,31 @@ const logout = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
 };
 
+const verifyEmail = async (req, res) => {
+  const { verificationToken, email } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new CustomError.UnauthenticatedError('Verification failed');
+  }
+
+  if (user.verificationToken !== verificationToken) {
+    throw new CustomError.UnauthenticatedError('Verification failed');
+  }
+
+  user.isVerified = true;
+  user.verified = Date.now();
+  user.verificationToken = '';
+
+  await user.save();
+
+  res.status(StatusCodes.OK).json({ msg: 'Email verified' });
+};
+
 module.exports = {
   register,
   login,
   logout,
+  verifyEmail,
 };
---
+++ 11-auth-workflow/starter/server/utils/nodemailerConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+  host: 'smtp.ethereal.email',
+  port: 587,
+  auth: {
+    user: 'hester.harber64@ethereal.email',
+    pass: 'V355v3CdjsNbJBFUmh',
+  },
+};
